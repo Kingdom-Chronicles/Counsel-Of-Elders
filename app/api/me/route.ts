d4c1e4ab2de167fc442a57df5ab9c05c683c8ef9
masikotimo <masikotimo@gmail.com>
2025-03-24 16:00:37 +0300

    Try to fix auth

--- app/api/me/route.ts
+++ app/api/me/route.ts
@@ -1,11 +1,11 @@
+import { getSession } from "next-auth/react"
 import { NextResponse } from "next/server"
 
 import { db } from "@/lib/db"
-import { auth } from "@/auth"
 
 
 export async function GET() {
-  const session = await auth()
+  const session = await getSession()
 
   if (!session || !session.user) {
     return NextResponse.json({ error: "Not authenticated" }, { status: 401 })
